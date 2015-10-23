04d47b297c43f062b4598cad324d09ffb23c98dd
perlancar (@pc-office) <perlancar@gmail.com>
2015-10-23 13:24:01 +0700

    Add some properties

--- lib/Acme/CPANLists/Test.pm
+++ lib/Acme/CPANLists/Test.pm
@@ -5,21 +5,24 @@ package Acme::CPANLists::Test;
 
 our @Module_Lists = (
     {
+        id => 'cba525a5-436c-364f-b5d0-6d8bda85b386',
         summary => 'Test list',
         entries => [
-            {module=>'Foo::Bar', summary=>'bar'},
-            {module=>'Foo::Baz', summary=>'baz'},
+            {module=>'Foo::Bar', summary=>'bar', related_modules=>['Foo::Qux']},
+            {module=>'Foo::Baz', summary=>'baz', alternate_modules=>['Foo::Quux', 'Foo::Corge']},},
         ],
     },
 );
 
 our @Author_Lists = (
     {
+        id => '43151a18-dcf5-873b-ad35-1486c8925cb6',
         summary => 'Test list',
         entries => [
             {author=>'BARBIE'},
             {author=>'NEILB'},
             {author=>'RJBS'},
+            {author=>'PERLANCAR', related_authors=>['SHARYANTO']},
         ],
     },
 );
